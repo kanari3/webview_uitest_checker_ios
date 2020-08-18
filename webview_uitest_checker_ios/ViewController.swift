8d4e992c0f8ca6a7d18b43a23325bd39906c73e6
kanari3 <kanari333333@gmail.com>
2020-08-19 01:28:08 +0900

    create project

---
+++ webview_uitest_checker_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webview_uitest_checker_ios
+//
+//  Created by kanari3 on 2020/08/19.
+//  Copyright © 2020 kanari3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
