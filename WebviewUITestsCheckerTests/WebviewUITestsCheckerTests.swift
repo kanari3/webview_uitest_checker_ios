b8a4ec463d60ee9068644e130fbd5bc3272544bf
kei.kitajima <kei.kitajima@88oct.co.jp>
2020-08-20 17:32:01 +0900

    remake

--- WebviewUITestsCheckerTests/WebviewUITestsCheckerTests.swift
+++ WebviewUITestsCheckerTests/WebviewUITestsCheckerTests.swift
@@ -2,7 +2,7 @@
 //  WebviewUITestsCheckerTests.swift
 //  WebviewUITestsCheckerTests
 //
-//  Created by kanari3 on 2020/08/19.
+//  Created by admin on 2020/08/20.
 //  Copyright © 2020 kanari3. All rights reserved.
 //
 
@@ -24,5 +24,11 @@ class WebviewUITestsCheckerTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
 
 }
--- WebviewUITestsCheckerUITests/WebviewUITestsCheckerUITests.swift
+++ WebviewUITestsCheckerUITests/WebviewUITestsCheckerUITests.swift
@@ -1,35 +1,43 @@
 //
-//  webview_uitest_checker_iosUITests.swift
-//  webview_uitest_checker_iosUITests
+//  WebviewUITestsCheckerUITests.swift
+//  WebviewUITestsCheckerUITests
 //
-//  Created by kanari3 on 2020/08/19.
+//  Created by admin on 2020/08/20.
 //  Copyright © 2020 kanari3. All rights reserved.
 //
 
 import XCTest
 
-class webview_uitest_checker_iosUITests: XCTestCase {
+class WebviewUITestsCheckerUITests: XCTestCase {
 
-    let app = XCUIApplication()
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
 
-    override func setUp() {
+        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
         app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testTextFields() {
-
-        let textFieldsCount = app.webViews.textFields.count
-        XCTAssertEqual(2, textFieldsCount)
-
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+                XCUIApplication().launch()
+            }
+        }
     }
-
-    func testButtons() {
-
-        let buttonsCount = app.webViews.buttons.count
-        XCTAssertEqual(2, buttonsCount)
-
-    }
-
-
 }
